9aa0d020aed20514395bc54bf9c024745634fb1a
Psi505 <Psi505@protonmail.com>
2022-10-13 22:36:41 +0200

    issues fix

--- routersploit-installer.sh
+++ routersploit-installer.sh
@@ -10,17 +10,17 @@ clear_font=`echo -n '\e[00m'`;
 # Header
 clear
 echo -e "";
-echo -e "$yellow╔═════════════════════════════════════════════════════════════════╗$clear_font";
-echo -e "$yellow║\t\t\t\t\t\t\t\t  ║$clear_font";
-echo -e "$yellow║$clear_font$blue\t\t Routersploit installer for termux\t\t  $clear_font$yellow║$clear_font";
-echo -e "$yellow║\t\t\t\t\t\t\t\t  ║\e[00m";
-echo -e "$yellow╚═════════════════════════════════════════════════════════════════╝$clear_font";
+echo -e "$yellow╔═════════════════════════════════════════════════╗$clear_font";
+echo -e "$yellow║\t\t\t\t\t\t  ║$clear_font";
+echo -e "$yellow║$clear_font$blue\t Routersploit installer for termux \t  $clear_font$yellow║$clear_font";
+echo -e "$yellow║\t\t\t\t\t\t  ║\e[00m";
+echo -e "$yellow╚═════════════════════════════════════════════════╝$clear_font";
 echo -e "";
 
 
 # Set up files
-echo -e "cryptography==2.8 \nfuture \nrequests==2.21.0 \nparamiko \npysnmp==4.4.6 \npycryptodome" >>requirements.txt
-echo -e "cryptography==2.8 \nfuture \nrequests==2.21.0 \nparamiko \npysnmp==4.4.6 \npycryptodome \npytest==4.4.0 \npytest-forked \npytest-xdist \nflake8 \ngit+git://github.com/threat9/threat9-test-bed" >>requirements-dev.txt
+echo -e "cryptography==3.3.2 \nfuture \nrequests==2.21.0 \nparamiko \npysnmp==4.4.6 \npycryptodome \nbcrypt==3.2.2 " >>requirements.txt
+echo -e "cryptography==3.3.2 \nfuture \nrequests==2.21.0 \nparamiko \npysnmp==4.4.6 \npycryptodome \nbcrypt==3.2.2 \npytest==4.4.0 \npytest-forked \npytest-xdist \nflake8 \ngit+https://github.com/threat9/threat9-test-bed.git" >>requirements-dev.txt
 
 echo -e "$yellow[!]$clear_font Updating termux packages.";
 sleep 2
@@ -28,12 +28,13 @@ apt update -y && apt upgrade -y
 
 echo -e "$yellow[!]$clear_font Installing required packages.";
 sleep 2
-apt -y install autoconf automake bison bzip2 clang cmake coreutils diffutils flex gawk git grep gzip libtool make patch perl sed silversearcher-ag tar wget pkg-config python clang libcrypt libffi git openssl libsodium
+apt -y install autoconf automake bison bzip2 clang cmake coreutils diffutils flex gawk git grep gzip libtool make patch perl sed silversearcher-ag tar wget pkg-config python clang libcrypt libffi git openssl libsodium rust
 
 echo -e "$yellow[!]$clear_font Installing required python modules.";
 sleep 2
 python -m pip install --upgrade pip
 python -m pip install wheel
+python -m pip install -U pip setuptools
 SODIUM_INSTALL=system pip install pynacl
 pip install -r requirements.txt
 pip install -r requirements-dev.txt
